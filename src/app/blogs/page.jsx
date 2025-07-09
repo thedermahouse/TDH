94740cc1869fdb134206629def6623acd69d9a3b
Aditya NArkar <narkaraditya10@gmail.com>
2025-07-09 16:18:56 +0530

    changes

--- src/app/blogs/page.jsx
+++ src/app/blogs/page.jsx
@@ -41,7 +41,7 @@ export default async function BlogsPage() {
         <section className="py-16">
           <div className="container mx-auto px-4">
             <h1 className="font-hallengerSerif text-4xl text-center mb-12">
-              Your Blogs
+              Our Blogs
             </h1>
             <p className="text-center text-base-content opacity-75">
               No blogs available at the moment.
