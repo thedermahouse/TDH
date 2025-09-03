5783ed8befd3972a60de0e9e2def6226f248314d
AdityanTW <adityan.theworks@gmail.com>
2025-09-03 14:57:37 +0530

    test

--- src/pages/api/landing-pages/[id]/faqs/index.js
+++ src/pages/api/landing-pages/[id]/faqs/index.js
@@ -22,5 +22,5 @@ export default async function handler(req, res) {
     return res.status(200).json({ message: "FAQ created" });
   }
 
-  return res.status(405).json({ message: "Method not allowed" });
+  return res.status(405).json({ message: "Method not allowed" }); 
 }
