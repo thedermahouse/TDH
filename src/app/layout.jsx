ec480a7b2261aa2763ee62db75e28820301f2cd3
AdityanTW <adityan.theworks@gmail.com>
2025-11-01 11:08:49 +0530

    gtm update

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -99,7 +99,7 @@ export default function RootLayout({ children }) {
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-N6Q4M6DT');
+            })(window,document,'script','dataLayer','GTM-MRQZ72GT');
           `}
         </Script>
         <Script
@@ -127,9 +127,9 @@ export default function RootLayout({ children }) {
         className={`antialiased ${montserrat.variable} ${hallengerSerif.variable}`}
       >
         {/* Google Tag Manager (noscript) */}
-        <noscript>
+         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-N6Q4M6DT"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MRQZ72GT"
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
